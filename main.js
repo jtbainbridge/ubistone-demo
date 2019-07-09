eab23bd22cf9ceefbacbae7e2e167cfbc203d3be
James Bainbridge <jamestbainbridge@icloud.com>
2019-07-09 11:02:00 +0100

    Add JS to control theme button and screenshot

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ const burgerMenu = document.querySelector('.burger');
 menuBtn.onclick = function () {
     burgerMenu.classList.toggle('visible');
 }
+
+// Theme selector
+function themeSelect() {
+  const lightBtn = document.getElementById('light');
+  const darkBtn = document.getElementById('dark');
+  const img = document.getElementById('screenshot');
+
+  lightBtn.addEventListener('click', function () {
+      lightBtn.classList.add('active');
+      darkBtn.classList.remove('active');
+      img.setAttribute('src', 'assets/screenshot-light.png');
+  });
+
+  darkBtn.addEventListener('click', function () {
+      darkBtn.classList.add('active')
+      lightBtn.classList.remove('active');
+      img.setAttribute('src', 'assets/screenshot-dark.png');
+  });
+};
+
+window.onload = themeSelect();
